01194c916f115c18eb79b46d59aa0233be05c02f
AndreyPomaz <andreypomaz@gmail.com>
2023-04-06 20:49:26 +0700

    branch2 -commit4 - total 7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         test_2.print();
-    }
 
+        System.out.println("Hello world?");
+    }
 }
